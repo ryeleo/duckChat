4c74a846287926994c953b44c8b8ca45beadc1c3
Ryan Leonard <ryan@cs.uoregon.edu>
2012-10-15 20:46:00 -0700

    included the appropriate local header files in client.c and server.c

--- src/client.c
+++ src/client.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+#include "duckchat.h"
+#include "raw.h"
+
 int main(int argc, char *argv[]){
-    printf("Hello World!\n");
+    char buffer[32];    
+    raw_mode();
+    scanf("%s", buffer);
+    printf("this my shit: %s\n", buffer);
+    cooked_mode();  
 }
