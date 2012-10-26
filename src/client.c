2d48425e41eda4e6f8e740659a70412e0647268f
Ryan Leonard <ryan@cs.uoregon.edu>
2012-10-26 15:51:48 -0700

    client login functionality

--- src/client.c
+++ src/client.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <string.h>
 #include <netdb.h>
+#include <unistd.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -9,16 +10,42 @@
 #include "duckchat.h"
 #include "raw.h"
 
+#define STDIN 1
+#define STDOUT 0
+#define CHAR_AMOUNT 1
+#define BUFF_SIZE 32
 
 int main(int argc, char *argv[]){
 
-    int ret;
-    int sockfd; // FD for primary client socket
-    char *server, *port, *uname;
-    struct addrinfo hints, *serv, *servIter;  
-
-    struct request_login packLogin;
+    int ret,            // general return variable 
+        i,              // general counter
+        sockfd;         // file descriptor for client socket
     
+    char 
+        *server,        // domain name of the server the client will connect to
+        *port,          // port number on server-end
+        *uname,         // username the user requests
+
+        inputBuf[32],   // buffer input from command line
+        inputChar,      // used along with buffer for user input 
+    
+        token1[32],     // used in parsing client requests
+        token2[32],     // used in parsing client requests 
+
+        activeChannel[32],  // name of channel user is on 
+        **channels;     // list of channels user can intera
+   
+     struct addrinfo 
+        hints,          
+        *serv, 
+        *servIter;  
+
+    struct request_login 
+        packLogin;
+    
+    struct 
+    
+     
 // (1) Client Initialization
     
     // verfiy input params 
@@ -34,7 +61,7 @@ int main(int argc, char *argv[]){
   
     // initialize hints for call to getaddrinfo
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC; 
+    hints.ai_family = AF_INET; 
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_protocol = 0;
     
@@ -44,10 +71,15 @@ int main(int argc, char *argv[]){
         printf("Error occurred during getaddrinfo: errorcode(%d)\n",ret);  
         return 1;
     }
- 
+     
     // attempt to create our socket matching the addrinfo given
-    for(servIter = serv ; sockfd == -1 && servIter != NULL  ; servIter = servIter->ai_next)
-        sockfd = socket(servIter->ai_family, servIter->ai_socktype, servIter->ai_protocol);
+    sockfd = -1;
+    servIter = serv;	
+    while(sockfd == -1 && servIter != NULL ){
+	sockfd = socket(servIter->ai_family, servIter->ai_socktype, servIter->ai_protocol);
+        if (sockfd == -1)	
+            servIter = servIter->ai_next;
+    }
     if (sockfd == -1){
         printf("Could not establish socket: %s\n", strerror(errno));
         return 1;
@@ -72,14 +104,15 @@ int main(int argc, char *argv[]){
     packLogin.req_type = REQ_LOGIN;
     strncpy(packLogin.req_username, uname, USERNAME_MAX);
 
-    // send the login message 
-    ret = write(sockfd, packLogin, sizeof(packLogin));
+    // send the login message
+     
+    ret = write(sockfd, (char*) &packLogin, sizeof(packLogin));
     if (ret == -1){
         printf("Error occurred at login: %s\n", strerror(errno));
         return 1;
     }
     
-// (4, 5) Main event loop
+// (4, 5, 6) Main event loop
     ret = raw_mode();
     if (ret != 0){
         printf("Issue entering raw mode");
@@ -87,10 +120,46 @@ int main(int argc, char *argv[]){
     }
     while(1){//begin Main event loop
 
-// (2) Read/Parse user commands
+// (4) Get user input
+        memset(inputBuf, '\0', BUFF_SIZE);
+        i = 0;
+        inputChar = '\0';
+        do{
+            ret = read(STDIN, &inputChar, CHAR_AMOUNT);    
+            if (ret != CHAR_AMOUNT){
+                printf("Error occurred during user input: %s\n", strerror(errno));
+                return 1;
+            }
+            if (i >= 31)
+                continue;
+            else{
+                inputBuf[i] = inputChar;
+                write(STDOUT, &(inputBuf[i]), CHAR_AMOUNT);   
+            }
+            i++;
+        }while(inputChar != '\n');
 
-// (3) Send user command to server
+// (5) Parse user input and send to server
+        token1 = strtok(inputBuffer, ' ');
+        token2 = strtok(NULL, ' ');
+        //TODO: Impliment all commands
+        if(inputBuf[0] == '/' ){ // send command
+            if(strcmp("/exit", token1) == 0){
+            }else if(strcmp("/join", token1)){ //check token 2
+            }else if(strcmp("/leave", token1)){ //check token 2
+            }else if(strcmp("/list", token1)){
+            }else if(strcmp("/who", token1)){ // check token 2
+            }else if(strcmp("/switch", token1)){ // check token 2
+            }else{ //wrong
+            }
+        }else{ // say
+            //XXX: Impliment say
+        }
+// (6) Send the request to the server
 
     }//end Main event loop
     cooked_mode();  
+}
+
+char *parse(char *input){
 }
